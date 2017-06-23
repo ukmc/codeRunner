97b42a13345e0bb4e7d81553085d99d3b005a051
Utku Kamacı <ukmc.j0@gmail.com>
2017-06-23 19:26:24 +0300

    bugfix, js implemented, website works fine!

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function updateOutput() {
 	$("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() 
 		+ "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+	document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+	
 }
 
 
