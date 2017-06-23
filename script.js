52f5f0d8d3ef10b22b5b862e81e25576cb85910f
Utku Kamacı <ukmc.j0@gmail.com>
2017-06-23 18:51:57 +0300

    Html conversion completed

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ $(".toggleButton").click(function() {
 	$(this).removeClass("highlightedButton");
 
 });
+
+$(".panel").height($(window).height() - $("header").height() - 15);
+
+$(".panel").width(($(window).width() / 2) -10);
+
+$("iframe").contents().find("html").html($("#htmlPanel").val());
+
+$("textarea").on("change keyup paste", function() {
+
+	$("iframe").contents().find("html").html($("#htmlPanel").val());
+
+});
